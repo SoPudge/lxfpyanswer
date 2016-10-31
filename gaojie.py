3e117643dc963441b367bfc5587f2bbc1d7ed7b7
SoPudge <abcd666@gmail.com>
2016-10-31 16:47:46 +0800

    modified in 161031

--- gaojie.py
+++ gaojie.py
@@ -64,3 +64,8 @@ print(L2)
 def prod(L):
     return reduce(lambda x,y:x*y,L)
 print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+#利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
+def str2float(s):
+    def fn(x,y):
+        return x*10+y
+    def str2float: 
