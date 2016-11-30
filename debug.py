3bbf77de68b99ec7183f815805b84585e7d1aa0b
SoPudge <abcd666@gmail.com>
2016-11-30 09:35:00 -0500

    modified in 161130

--- debug.py
+++ debug.py
@@ -5,7 +5,7 @@ def foo(s):
     return 10 / n
 def main():
     foo('0')
-main()
+#main()
 #可以用print来打印调试信息，确认程序的错误信息
 def foo(s):
     n = int(s)
@@ -13,5 +13,18 @@ def foo(s):
     return 10 / n
 def main():
     foo('0')
-main()
-#assert是断言的意思是断言后面跟的内容应该是TRUE，否则断言下面的内容就会出错
+#main()
+#assert是断言的意思是断言后面跟的内容如果满足，则什么都不做，否则执行显示断言后半段内容
+#此处n=0的话，则断言不成立，那么执行n is zero
+import logging
+logging.basicConfig(level = logging.INFO)
+s = '0'
+n = int(s)
+logging.info('n = % d' % n)
+print(10 / n)
+#logging输出错误的信息
+s = '0'
+n = int(s)
+print(10/n)
+#pdb可以单步执行代码，按n单步执行代码，按p可以显示变量
+
