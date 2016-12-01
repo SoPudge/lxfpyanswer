5dce382c3e1140d9972c9a56093cc54a66177eb2
SoPudge <abcd666@gmail.com>
2016-12-01 16:34:35 +0800

    modified in 161201

--- uinttest.py
+++ uinttest.py
@@ -1,3 +1,24 @@
 # -*- coding: utf-8 -*-
+class Dict(dict):
+    def __init__(self,**kw):
+        super().__init__(**kw)
+    def __getattr__(self,key):
+        try:
+            return self[key]
+        except KeyError:
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+    def __setattr__(self,key,value):
+        self[key] = value
 
-
+import unittest
+class TestDict(unittest.TestCase):
+    def test_init(self):
+        d = Dict(a = 1,b = 'test')
+        self.assertEqual(d.a,1)
+        self.assertEqual(d.b,'test')
+        self.assertTrue(isinstance(d,dict))
+    def test_key(self):
+        d = Dict()
+        d['key'] = 'value'
+        self.assertEqual(d.key,'value')
+    def test_attr(self)
