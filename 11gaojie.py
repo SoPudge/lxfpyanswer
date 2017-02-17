175b9230becf9ac2e05221c1203cf430417a8ecc
SoPudge <abcd666@gmail.com>
2017-02-17 09:19:22 +0800

    20170217

--- 11gaojie.py
+++ 11gaojie.py
@@ -74,7 +74,15 @@ def prod(L):
 print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
 #利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456：
 def str2float(s):
-    def fn(x,y):
-        return 
+    def f1(x,y):
+        return x*10 + y
+    def f2(x,y):
+        return x/10 + y
     def str2num(m):
-        return {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[m]
+        return {'.': 0,'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}[m]
+    def fenli(s):
+        sz = [x for x in s if(x = '.') break]
+    numlst = map(str2num,s)
+    dotnum = dot(s)
+    sz = 
+
