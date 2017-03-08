e899f6ef5070ad7673aec23d468e37f22f36309c
SoPudge <abcd666@gmail.com>
2017-03-08 22:22:51 +0800

    20170308

--- 38multiprocess.py
+++ 38multiprocess.py
@@ -2,3 +2,21 @@
 import os
 print('Process(%s) start...' % os.getpid())
 pid = os.fork()
+if pid == 0:
+    print('i am a child process (%s) and my parent is %s' % (os.getpid(),os.getppid()))
+else:
+    print('i (%s) just created a child process(%s)' % (os.getpid,pid))
+
+from multiprocessing import Process
+import os
+
+def run_proc(name):
+    print('run child process %s (%s)...' % (name,os.getpid()))
+
+if __name__ =='__main__':
+    print('Parent process %s' % os.getpid())
+    p = Process(target=run_proc,args=('test',))
+    print('child process will start.')
+    p.start()
+    p.join()
+    print('child process end')
