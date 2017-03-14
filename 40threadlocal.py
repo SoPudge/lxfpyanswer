2f84c9441aca140b8c73ed70be12cfd2c198accf
SoPudge <abcd666@gmail.com>
2017-03-14 16:55:20 +0800

    20170314

--- 40threadlocal.py
+++ 40threadlocal.py
@@ -13,10 +13,15 @@ def do_task_2(std):
 #所以线程中推荐使用自己的局部变量，可以用一个dict存储对应的变量
 import threading
 local_school = threading.local()
-def process_student()；
-std = local_school.student
-print('Hello %s (in %s)' % (std,threading.current_thread().name))
+def process_student():
+    std = local_school.student
+    print('Hello %s (in %s)' % (std,threading.current_thread().name))
 def process_thread(name):
-    local_school.student = name
+    local_school.student = name#这里是给local_school绑定了student属性
     process_student()
-t1 = threading.Thread(target = process_thread,)
+t1 = threading.Thread(target = process_thread,args=('Alice',),name='Thread-A')
+t2 = threading.Thread(target = process_thread,args=('Bob',),name='Thread-B')
+t1.start()
+t2.start()
+t1.join()
+t2.join()
