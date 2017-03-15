7b420c6fa741c48e7f72a3b8355883d5052f7708
SoPudge <abcd666@gmail.com>
2017-03-15 20:13:04 +0800

    20170315

--- 43datetime.py
+++ 43datetime.py
@@ -26,4 +26,42 @@ now = datetime.now()
 print(now.strftime('%A,%B %d %H:%M'))
 #这里同样将标准时间转换成字符串
 
+from datetime import datetime,timedelta
+now = datetime.now()
+print(now)
+datetime(2015,5,18,16,57,3,540977)
+print(now + timedelta(hours = 10))
+print(now + timedelta(hours = 10,minutes = 20))
+#通过timedelta可以方便的加减时间，而不用进行换算
 
+from datetime import timezone
+tz_utc_8 = timezone(timedelta(hours = 7))
+now = datetime.now()
+print(now) 
+dt = now.replace(tzinfo = tz_utc_8) 
+print(dt)
+#本地时间转换成UTC时间
+
+utc_dt = datetime.utcnow().replace(tzinfo=timezone.utc)
+print(utc_dt)
+#这是拿到UTC时间
+bj_dt = utc_dt.astimezone(timezone(timedelta(hours = 8)))
+print(bj_dt)
+#将UTC时间转换成北京时间
+ty_dt = bj_dt.astimezone(timezone(timedelta(hours = 9)))
+print(ty_dt)
+#将北京时间转换成东京时间，可见只要时区正确，可以随意转换
+
+#假设你获取了用户输入的日期和时间如2015-1-21 9:01:30，以及一个时区信息如UTC+5:00，均是str，请编写一个函数将其转换为timestamp：
+import re
+from datetime import datetime,timezone,timedelta
+def to_timestamp(dt_str,tz_str):
+
+
+t1 = to_timestamp('2015-6-1 08:10:30', 'UTC+7:00')
+assert t1 == 1433121030.0, t1
+
+t2 = to_timestamp('2015-5-31 16:10:30', 'UTC-09:00')
+assert t2 == 1433121030.0, t2
+
+print('Pass')
