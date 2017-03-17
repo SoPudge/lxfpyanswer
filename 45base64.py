08405489836b6dbde2c4326f4ce4ad9aaa9ae69f
SoPudge <abcd666@gmail.com>
2017-03-17 15:01:41 +0800

    20170317

--- 45base64.py
+++ 45base64.py
@@ -10,3 +10,14 @@ print(base64.urlsafe_b64encode(b'i\xb7\x1d\xfb\xef\xff'))
 #可以在URL当中加密内容，比如中文
 print(base64.urlsafe_b64decode(b'abcd--__'))
 print(base64.urlsafe_b64encode(b'zhongqi.zhang'))
+
+def safe_base64_decode(s):
+    i = len(s) % 4
+    if i == 0:
+        return base64.urlsafe_b64decode(s)
+    else:
+        return base64.urlsafe_b64decode(s+(4-i)*b'=')
+assert b'abcd' == safe_base64_decode(b'YWJjZA=='), safe_base64_decode('YWJjZA==')
+assert b'abcd' == safe_base64_decode(b'YWJjZA'), safe_base64_decode('YWJjZA')
+print('Pass')
+
