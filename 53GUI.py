c6c7f83128fc9d357b3dd1cc1d706de733d7565f
SoPudge <abcd666@gmail.com>
2017-03-25 18:10:17 +0800

    20170325

---
+++ 53GUI.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+from tkinter import *
+class Application(Frame):
+    def __init__(self,master=None):
+        Frame__init__(self,master)
+        self.pack()
+        self.createWidgets()
+    def createWidgets(self):
+        self.helloLabel = Label(self,text='hello world')
+        self.helloLabel.pack()
+        self.quitButton = Button(self,text='quit',command = self.quit)
+        self.quitButton.pack()
