f154685d9b0487a33aab972e62de4945f7d8aa0a
SoPudge <abcd666@gmail.com>
2017-03-27 09:10:14 +0800

    '20170327'

--- 53GUI.py
+++ 53GUI.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*- 
 from tkinter import *
+import tkinter.messagebox as messagebox
 class Application(Frame):
     def __init__(self,master=None):
-        Frame__init__(self,master)
+        Frame.__init__(self,master)
         self.pack()
         self.createWidgets()
     def createWidgets(self):
@@ -13,3 +14,25 @@ class Application(Frame):
 app = Application()
 app.master.title('Hello World')
 app.mainloop()
+#tkinter是python的标准库，记住首先初始化一个大窗口，即init方法，然后再初始化布局
+#每创建一个组件，需要用self.pack()进行打包，负责显示
+#最后对图形界面进行实例化，设定标题等等
+
+class Application(Frame):
+    def __init__(self,master=None):
+        Frame.__init__(self,master)
+        self.pack()
+        self.createWidgets()
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self,text='Hello',command=self.hello)
+        self.alertButton.pack()
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message','Hello,%s' % name)
+app = Application()
+app.master.title('Hello World')
+app.mainloop()
+#实际上UI编程，可以在widget当中的command变量中定义执行的程序
+#先画UI，再执行方法程序
