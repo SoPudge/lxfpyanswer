85c8b3d74178eb8c7b64ac864ef6666fe52f9492
SoPudge <abcd666@gmail.com>
2017-04-04 20:16:04 +0800

    20170404

--- 56SMTP.py
+++ 56SMTP.py
@@ -1,14 +1,34 @@
 # -*- coding: utf-8 -*- 
+#from email.mime.text import MIMEText
+#msg = MIMEText('hello,send by Python...','plain','utf-8')
+#from_addr = input('From:')
+#password = input('Password:')
+#to_addr = input('To:')
+#smtp_server = input('SMTP server：')
+#
+#import smtplib
+#server = smtplib.SMTP(smtp_server,25)
+#server.set_debuglevel(1)
+#server.login(from_addr,password)
+#server.sendmail(from_addr,[to_addr],msg.as_string())
+#server.quit()
+#可以发送成功，但是返回错误代码554，意思是被识别成垃圾邮件，用的网易邮箱发送到QQ
+
+from email import encoders
+from email.header import Header
 from email.mime.text import MIMEText
-msg = MIMEText('hello,send by Python...','plain','utf-8')
+from email.utils import parseaddr,formataddr
+
+import smtplib
+
+def _format_addr(s):
+    name,addr = parseaddr(s)
+    return formataddr((Header(name,'utf-8').encode(),addr))
+
 from_addr = input('From:')
 password = input('Password:')
 to_addr = input('To:')
-smtp_server = input('SMTP server')
+smtp_server = input('SMTP server:')
 
-import smtplib
-server = smtplib.SMTP(smtp_server,25)
-server.set_debuglevel(1)
-server.login(from_addr,password)
-server.sendmail(from_addr,[to_addr],msg.as_string())
-server.quit()
+msg = MIMEText('hello,send by Python...','plain','utf-8')
+msg['From']
