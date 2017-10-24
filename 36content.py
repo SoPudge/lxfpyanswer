6a3d8224a827b1792ff1821f11ebc9234eadd804
SoPudge <abcd666@gmail.com>
2017-10-24 08:30:55 +0800

    '添加有关目录操作的注释'

--- 36content.py
+++ 36content.py
@@ -21,6 +21,7 @@ print('##########################################')
 print([x for x in os.listdir('.') if os.path.isfile(x)])
 #通过生成器来列表当前目录下所有文件，通过if来验证是否是文件还是目录
 print([x for x in os.listdir() if os.path.isfile(x) and os.path.splitext(x)[1] == '.py'])
+[print(x) for x in os.listdir() if os.path.isfile(x) and os.path.splitext(x)[1] == '.py']
 #这里os.path.splitext(x)[1]代表的是，该函数将文件名分割成一个list，包含两项，第一项是文件路径，第二项是后缀名
 #利用os模块编写一个能实现dir -l输出的程序。 #编写一个程序，能在当前目录以及当前目录的所有子目录下查找文件名包含指定字符串的文件，并打印出相对路径。 
 #[print(fname) for fname in os.listdir('.')] 
