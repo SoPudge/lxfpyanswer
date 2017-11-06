76819233068b9cf2c9d8f67e5381c2b2c0fd9302
SoPudge <abcd666@gmail.com>
2017-11-06 10:33:32 +0800

    '添加多进程说明'

--- 38multiprocess.py
+++ 38multiprocess.py
@@ -13,13 +13,13 @@ import os
 def run_proc(name):
     print('run child process %s (%s)...' % (name,os.getpid()))
 
-#if __name__ =='__main__':
-#    print('Parent process1 %s' % os.getpid())
-#    p = Process(target=run_proc,args=('test',))
-#    print('child process will start.')
-#    p.start()
+if __name__ =='__main__':
+    print('Parent process1 %s' % os.getpid())
+    p = Process(target=run_proc,args=('test',))
+    print('child process will start.')
+    p.start()
 #    p.join()
-#    print('child process end')
+    print('child process end')
 #注意，这里的父进程，就是文件执行本身的进程
 #通过Process创建子进程实质上就是用Process建立一个实例，其中target参数代表子进程调用的对象，args代表给调用对象传入的参数
 #对子进程实例进行p.start()操作，代表开始子进程，join方法代表阻塞当前进程，先调用子进程，执行完毕之后再执行后面的内容
@@ -42,6 +42,7 @@ print('##############################')
 #    p = Process(target=f, args=('bob',))
 #    p.start()
 #    p.join()
+#
 #这里在if后面就是按先后顺序执行，但将进程实例化成p之后，由于由join方法的存在，先执行子进程
 from multiprocessing import Pool
 import os,time,random
